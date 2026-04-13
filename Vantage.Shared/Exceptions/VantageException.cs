3d7d060cc0001915cda09d27cd7d5357746566f0
Lucas Pagano Polisel <lucaspaganopolisel@gmail.com>
2026-04-13 17:32:05 -0500

    build(core): create slnx solution, scaffold shared project, and implement rfc 9457 exception base

---
+++ Vantage.Shared/Exceptions/VantageException.cs
@@ -0,0 +1,8 @@
+﻿namespace Vantage.Shared.Exceptions
+{
+    public abstract class VantageException(string title, string message, int statusCode) : Exception(message)
+    {
+        public string Title { get; } = title;
+        public int StatusCode { get; } = statusCode;
+    }
+}
